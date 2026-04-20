fd0068f3777f6314641918c965902fc3fbfaa65b
Rashmi1bhatt <32875325+Rashmi1bhatt@users.noreply.github.com>
2026-04-20 16:02:22 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import "./App.css";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <div style={{ paddingTop: "70px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
