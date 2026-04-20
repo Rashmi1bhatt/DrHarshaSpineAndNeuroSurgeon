7887e691e62be633632da0ffb514415bd2daae10
Rashmi1bhatt <32875325+Rashmi1bhatt@users.noreply.github.com>
2026-04-20 15:59:49 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,22 @@
+export default function Home() {
+  return (
+    <div className="page">
 
+      <section className="hero">
+        <h1>Dr. Harsha Narayanamurthy</h1>
+        <p>Senior Consultant – Neurosurgery</p>
+        <p>17+ years experience | 2500+ surgeries</p>
+
+        <div className="buttons">
+          <a href="https://www.kauveryhospitalsbangalore.com/book-an-appointment?hospital_ids=1&speciality_ids=4,27,42&doctor_name=Dr.%20Harsha%20Narayanamurthy">
+            Book at Kauvery
+          </a>
+          <a href="https://www.apolloclinic.com/find-a-doctor/best-neuro-surgeon-in-bengaluru-karnataka/dr-harsha-narayanamurthy">
+            Book at Apollo
+          </a>
+        </div>
+      </section>
+
+    </div>
+  );
+}
