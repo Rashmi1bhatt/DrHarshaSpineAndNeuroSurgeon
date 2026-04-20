b2cb8b08d4a3c884bf0738d3cb85eef57bb2e68a
Rashmi1bhatt <32875325+Rashmi1bhatt@users.noreply.github.com>
2026-04-20 15:49:20 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,25 @@
+import "./Navbar.css";
 
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Dr. Harsha</h2>
+
+      <div className="nav-links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+      </div>
+
+      <div className="nav-buttons">
+        <a href="https://www.kauveryhospitalsbangalore.com/book-an-appointment?hospital_ids=1&speciality_ids=4,27,42&doctor_name=Dr.%20Harsha%20Narayanamurthy">
+          Book (Kauvery)
+        </a>
+
+        <a href="https://www.apolloclinic.com/find-a-doctor/best-neuro-surgeon-in-bengaluru-karnataka/dr-harsha-narayanamurthy">
+          Book (Apollo)
+        </a>
+      </div>
+    </nav>
+  );
+}
